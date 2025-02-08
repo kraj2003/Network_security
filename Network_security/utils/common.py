af8050f699d9a71c96d15cbb4c7addc3c6d652c0
Khushi <khushirajpurohit617@gmail.com>
2025-02-08 18:19:33 +0530

    Data Validation completed

---
+++ Network_security/utils/common.py
@@ -0,0 +1,127 @@
+import os
+import yaml
+import json
+# from box import ConfigBox
+import joblib # let us save a model 
+from ensure import ensure_annotations
+import pickle
+import sys
+from pathlib import Path
+from typing import Any
+import numpy as np
+from Network_security.exception.exception import NetworkSecurityException
+from Network_security.logging.logger import logging
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
+@ensure_annotations
+def read_yaml(path_to_yaml: str) -> dict:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logging.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return content
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def save_numpy_array_data(file_path: str, array: np.array):
+        """
+        Save numpy array data to file
+        file_path: str location of file to save
+        array: np.array data to save
+        """
+        try:
+            dir_path = os.path.dirname(file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            with open(file_path, "wb") as file_obj:
+                np.save(file_obj, array)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys) from e
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def load_object(file_path: str, ) -> object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file: {file_path} is not exists")
+        with open(file_path, "rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+    
+
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Network_security.components.data_ingestion import DataIngestion
 from Network_security.exception.exception import NetworkSecurityException
 from Network_security.logging.logger import logging
+from Network_security.components.data_validation import DataValidation
 import sys
-from Network_security.entity.config_entity import DataIngestionConfig
+from Network_security.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from Network_security.entity.artifact_entity import DataIngestionArtifact
 from Network_security.entity.config_entity import TrainingPipelineConfig
 
@@ -14,7 +15,14 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("Initiate data ingestion")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data_intialization completed")
         print(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Initiated data validation")
+        data_validation_artifact=data_validation.initaiate_data_validation()
+        logging.info("data_validation_completed")
+        print(data_validation_artifact)
     except Exception as e:
         raise NetworkSecurityException(e,sys)
         
